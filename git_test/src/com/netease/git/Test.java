4f477f485f383e9b5ecc6947a3e482026fcbe661
xpliu <xpliu@corp.netease.com>
2014-10-30 13:47:03 +0800

    [git] test xpliu

---
+++ git_test/src/com/netease/git/Test.java
@@ -0,0 +1,18 @@
+package com.netease.git;
+
+public class Test {
+	private int i;
+	private int j;
+	
+	public Test() {
+	}
+	
+	public Test(int i,int j) {
+		this.i = i;
+		this.j = j;
+	}
+	
+	public int add(){
+		return i+j;
+	}
+}
